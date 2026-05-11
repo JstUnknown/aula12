0dc1f1a8f35fbe9d3ce79c0c187d378c173c13bb
Unknown <120033812+JstUnknown@users.noreply.github.com>
2026-05-11 17:11:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import cv2
+import mediapipe as mp
+from pynput.keyboard import Key,Controller
+
+keyboard=Controller()
+
+cap = cv2.VideoCapture(0)
+
+width=int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height=int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(
+    min_detection_confidence=0.8,
+    min_tracking_confidence=0.5
+)
+
+tips = [4, 8, 12, 16, 20]
+state=None
+
+# FUNÇÃO PRA CONTAR DEDOS
+def count_fingers(hand_landmarks, handNo=0):
+    fingers = []
+    
+    global state
+    if hand_landmarks:
+        landmarks=hand_landmarks[handNo].landmark
+        for lm_index in tipIds:
+            fingers_tip_y=landmarks[lm_index].y
+            fingers_bottom_y=landmarks[lm_index-2].y
+            if lm_index!=4:
+                if fingers_tip_y<fingers_bottom_y:
+                    fingers.append(1)
+                if fingers_tip_y>fingers_bottom_y:
+                    fingers.append(0)
+
+        totalFingers=fingers.count(1)
+        if totalFingers==4:
+            state="Play"
+        if totalFingers==0 and state=="Play":
+            state="Pause"
+            keyboard.press("K")
+        fingers_tip_x=(landmarks[8].x)*width
+        if totalFingers==1:
+            if fingers_tip_x<width-400:
+                keyboard.press(Key.left)
+            if fingers_tip_x>width-50:
+                keyboard.press(Key.right)
+
+def drawHandLanMarks(image, hand_landmarks):
+    if hand_landmarks:
+        for landmarks in hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
+
+
+while True:
+    success, image = cap.read()
+
+    image = cv2.flip(image, 1)
+
+    # IMPORTANTE (MediaPipe precisa RGB)
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = hands.process(image_rgb)
+
+    hand_landmarks = results.multi_hand_landmarks
+    drawHandLanMarks(image,hand_landmarks)
+    count_fingers(image,hand_landmarks)
+
+    cv2.imshow("Controlador de Midia", image)
+
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
